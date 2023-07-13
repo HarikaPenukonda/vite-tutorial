fda3364640326a9d7a010fd4e615f0a07de8ef7a
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-13 16:58:24 -0500

    Create Toggle Component

---
+++ src/hooks/useContext/ToggleTheme.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ToggleTheme(){
+
+    return(
+        <>
+        <button>
+            Toggle Theme
+        </button>
+        </>
+        
+
+    )
+}
