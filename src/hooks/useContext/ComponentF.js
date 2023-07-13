b4a6bda3ee4e46fb85396d9282bbff095cc8177f
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-13 17:06:09 -0500

    consume the context value

--- src/hooks/useContext/ComponentF.js
+++ src/hooks/useContext/ComponentF.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { UserContext } from "./ComponentC";
 
 export default function ComponentF(){
     return(
         <>
-            
+            <UserContext.Consumer>
+                {
+                    user => {
+                        return <div> User Context value {user}</div>
+                    }
+                }
+            </UserContext.Consumer>
         </>
     )
 }
