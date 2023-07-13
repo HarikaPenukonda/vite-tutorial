8765c9bdba52d6139182d907dd16c2cfef9c9309
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-13 16:59:24 -0500

    import component C

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import ComponentC from "./hooks/useContext/ComponentC";
 // import UseStateHook from "./hooks/usestate/use-state";
 // import Counter from "./hooks/usestate/Counter";
 // import TextFeild from "./hooks/usestate/TextFeild";
 // import Checkbox from "./hooks/usestate/checkbox";
 // import Form from "./hooks/usestate/Form";
-import CounterUpdater from "./hooks/usestate/Counter-Updater";
+// import CounterUpdater from "./hooks/usestate/Counter-Updater";
+//import ToggleTheme from "./hooks/useContext/ToggleTheme";
 
 export default function App(){
     return(
-        <CounterUpdater/>
+        <ComponentC/>
+        // <ToggleTheme/>
+        // <CounterUpdater/>
         // <Form/>
         // <Checkbox/>
         // <TextFeild/>
