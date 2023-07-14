6e116735d6a99e0a6ae127efd5fd63c35aae667c
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-13 20:09:29 -0500

    Using useContext hooks

--- src/hooks/useContext/ComponentE.js
+++ src/hooks/useContext/ComponentE.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import ComponentF from "./ComponentF";
+import { UserContext, EmailContext } from "./ComponentC";
 
 export default function ComponentE(){
+
+    const user = useContext(UserContext)
+    const email = useContext(EmailContext)
     return(
         <>
-            <ComponentF/>
+         <ComponentF/>
+            <div>
+                <h3>{user} </h3>
+                <h3>{email}</h3> 
+                
+            </div>
         </>
     )
 }
